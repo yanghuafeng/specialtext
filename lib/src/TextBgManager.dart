7dee59bcbbb1db4ba74ec9bf140962877e92560d
user <1207378804@qq.com>
2023-12-28 15:01:37 +0800

    图片背景

--- lib/src/TextBgManager.dart
+++ lib/src/TextBgManager.dart
@@ -39,11 +39,7 @@ class TextBgManager {
   CachedVideoPlayerController? _videoController;
   List<String> localVideoList=["ludeng1.mp4","ludeng2.mp4","yuanshang.mp4",
     "humian.mp4","haipinmian.mp4","shamo.mp4",];
-  List<String> localImagesList=[
-    "1.png","2.png","3.png","4.png","5.png","6.png",
-    "7.png","8.png","9.png","10.png","11.png","12.png",/*"13.png",*/"14.png",
-    "15.png",/*"16.png",*/"17.png","18.png","19.png","20.png",
-    "21.gif","26.gif", "ludeng1.gif"];
+  List<String> localImagesList=["yuanshang.png", "ludeng.gif"];
 
   String? _name;
   bool _isFile = false;
